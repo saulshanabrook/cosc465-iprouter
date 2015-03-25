45baed9864299424dce4f6839b76fe6d00e07776
Saul Shanabrook <s.shanabrook@gmail.com>
2015-03-25 09:34:16 -0400

    project 3

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM jsommers/switchyard
+
+CMD ['-v', '-t', '-s', 'routertests1.srpy', 'myrouter.py']
--- myrouter.py
+++ myrouter.py
@@ -4,20 +4,17 @@
 Basic IPv4 router (static routing) in Python.
 '''
 
-import sys
-import os
-import time
 from switchyard.lib.packet import *
 from switchyard.lib.address import *
 from switchyard.lib.common import *
 
+
 class Router(object):
     def __init__(self, net):
         self.net = net
-        # other initialization stuff here
+        self.ip_to_mac = {interface.ipaddr: interface.ethaddr for interface in net.interfaces()}
 
-
-    def router_main(self):    
+    def router_main(self):
         '''
         Main method for router; we stay in a loop in this method, receiving
         packets until the end of time.
@@ -25,17 +22,26 @@ class Router(object):
         while True:
             gotpkt = True
             try:
-                dev,pkt = self.net.recv_packet(timeout=1.0)
+                dev, pkt = self.net.recv_packet(timeout=1.0)
             except NoPackets:
                 log_debug("No packets available in recv_packet")
                 gotpkt = False
             except Shutdown:
                 log_debug("Got shutdown signal")
                 break
-
             if gotpkt:
                 log_debug("Got a packet: {}".format(str(pkt)))
+                if pkt.has_header(Arp):
+                    arp = pkt.get_header(Arp)
+                    if arp.targetprotoaddr in self.ip_to_mac:
+                        reply = create_ip_arp_reply(
+                            srchw=self.ip_to_mac[arp.targetprotoaddr].raw,
+                            srcip=arp.targetprotoaddr,
+                            dsthw=arp.senderhwaddr,
+                            targetip=arp.senderprotoaddr,
 
+                        )
+                        self.net.send_packet(dev, reply)
 
 
 def switchy_main(net):
